f863d46dc9f1cf0606e5b2ad108aa5fbe1712ad4
RafaelLima99 <rafaellima.ti.99@gmail.com>
2020-11-23 17:45:11 -0200

    action.js

---
+++ public/js/action.js
@@ -0,0 +1,19 @@
+    //window.location captura o url da pagina
+    //o pathname, é toda url depois da "/"
+    //mas pode usar outros metodos que capturam a url completa
+    var url = window.location.pathname;
+    if(url == '/dashboard'){
+        $('#btn-dashboard').addClass('active')
+    }
+    else if(url == '/upload'){
+        $('#btn-upload').addClass('active')
+    }
+    else if(url == '/minhas-musicas'){
+        $('#btn-musicas').addClass('active')
+    }
+    else if(url == '/cadastro'){
+        $('#btn-cadastro').addClass('active')
+    }
+    else if(url == '/admins'){
+        $('#btn-admins').addClass('active')
+    }
