97c4b14abebbd4cb5bb9e2554cb3d405af8dec99
RafaelLima99 <rafaellima.ti.99@gmail.com>
2020-11-23 16:23:01 -0200

    Admin.php

---
+++ App/Models/Admin.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace App\Models;
+use MF\Model\Model;
+
+class Admin extends Model
+{
+    private $id;
+    private $nome;
+    private $email;
+    private $senha;
+    private $emailValido;
+    private $nivelAcesso;
+
+    public function login()
+    {
+        //verifica se o email do usuário existe no banco
+        $query = "SELECT * FROM tb_adm WHERE email = :email";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(':email', $this->email);
+        $stmt->execute();
+
+        //se existir
+        //verifica se a senha digitada é a mesma que condiz com o hash da senha que está no banco
+        if($stmt->rowCount()){
+            $usuario = $stmt->fetch();
+            if(password_verify($this->senha, $usuario['senha'])){
+                //insere o id e nome do usuário nos atribustos
+                $this->id   = $usuario['id'];
+                $this->nome = $usuario['nome'];
+                $this->nivelAcesso = $usuario['nivel_acesso'];
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function cadastra()
+    {
+        //se o e-mail a ser cadastrado for válido
+        if($this->verificaEmail()){
+            //cadastra os dados do usuário
+            $query = "INSERT INTO tb_adm (nome, email, senha, nivel_acesso) VALUES (:nome, :email, :senha, :nivel_acesso)";
+            $stmt = $this->db->prepare($query);
+            $stmt->bindValue(":nome",  $this->nome);
+            $stmt->bindValue(":email", $this->email);
+            $stmt->bindValue(":senha", $this->senha);
+            $stmt->bindValue(":nivel_acesso", $this->nivelAcesso);
+            $stmt->execute();
+            
+            $this->emailValido = true;
+
+        }else{
+            $this->emailValido = false;
+        }
+    }
+
+    //verifica se o email a ser cadastrado já existe no banco
+    //caso exista retorna false
+    public function verificaEmail()
+    {
+        $query = "SELECT * FROM tb_adm WHERE email = :email";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(':email', $this->email);
+        $stmt->execute();
+
+       if($stmt->rowCount()){
+            return false;
+       }else{
+           return true;
+       }   
+    }
+
+    public function getAdmin()
+    {
+        $query = "SELECT * FROM tb_adm";
+        $stmt = $this->db->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    public function removeAdm()
+    {
+        $query = "DELETE FROM tb_adm WHERE id = :id";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(':id', $this->id);
+        $stmt->execute();
+    }
+
+    //seters
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+     
+    public function setSenha($senha)
+    {
+        $this->senha = $senha;
+    }
+
+    public function setNivelAcesso($nivelAcesso)
+    {
+        $this->nivelAcesso = $nivelAcesso;
+    }
+ 
+     //geters
+     public function getId()
+    {
+        return $this->id;
+    }
+     public function getNome()
+    {
+        return $this->nome;
+    }
+
+    public function getEmailValido()
+    {
+        return $this->emailValido;
+    }
+
+    public function geNivelAcesso()
+    {
+        return $this->nivelAcesso;
+    }
+}
---
+++ App/Models/Musica.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Models;
+use MF\Model\Model;
+
+class Musica extends Model
+{
+    private $id;
+    private $id_adm;
+    private $id_genero;
+    private $musica;
+    private $autor;
+    private $arquivo;
+
+    public function salvar()
+    {
+        $query ='INSERT INTO tb_musicas(id_adm, id_genero, musica, autor, arquivo)
+                 VALUES(:id_adm, :id_genero, :musica, :autor, :arquivo)';
+
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(':id_adm',    $this->__get('id_adm'));
+        $stmt->bindValue(':id_genero', $this->__get('id_genero'));
+        $stmt->bindValue(':musica',    $this->__get('musica'));
+        $stmt->bindValue(':autor',     $this->__get('autor'));
+        $stmt->bindValue(':arquivo',   $this->__get('arquivo'));
+        $stmt->execute();
+    }
+
+    public function getTotalMusica()
+    {
+        $query = "SELECT COUNT(*) as total FROM tb_musicas WHERE id_genero = :id_genero";
+        $stmt  = $this->db->prepare($query);
+        $stmt->bindValue(':id_genero', $this->__get('id_genero'));
+        $stmt->execute();
+
+        return $stmt->fetch(\PDO::FETCH_OBJ)->total;
+    }
+
+    public function getTotalGenero()
+    {
+        //o GROUP BY está agrupando músicas quem tem o mesmo id_genero
+        $query = "SELECT COUNT(*) AS total, id_genero AS genero FROM tb_musicas GROUP BY id_genero";
+        $stmt  = $this->db->prepare($query);
+        $stmt->execute();
+
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    public function getPorGenero()
+    {
+        $query = "SELECT musica, autor, arquivo FROM tb_musicas 
+                  WHERE id_genero = :id_genero  ORDER BY id DESC" ;
+      
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(':id_genero',$this->__get('id_genero'));
+        $stmt->execute();
+
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    public function getPorAdm()
+    {
+        $query = "SELECT id, id_genero, musica, autor, arquivo FROM tb_musicas 
+                  WHERE id_adm = :id_adm ORDER BY id DESC";
+                  
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(':id_adm', $this->__get('id_adm'));
+        $stmt->execute();
+
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    public function getPorId()
+    {
+        $query = "SELECT id, musica, autor FROM tb_musicas WHERE id = :id";
+        $stmt  = $this->db->prepare($query);
+        $stmt->bindValue(':id', $this->__get('id'));
+        $stmt->execute();
+
+        return $stmt->fetch(\PDO::FETCH_ASSOC);
+    }
+
+    public function removeMusica()
+    {
+        $query = "DELETE FROM tb_musicas WHERE id = :id";
+        $stmt  = $this->db->prepare($query);
+        $stmt->bindValue(':id', $this->__get('id'));
+        $stmt->execute();
+    }
+
+    public function atualiza()
+    {
+        $query = "UPDATE tb_musicas SET musica = :musica, autor = :autor WHERE id = :id";
+        $stmt  = $this->db->prepare($query);
+        $stmt->bindValue(':musica', $this->__get('musica'));
+        $stmt->bindValue(':autor',  $this->__get('autor'));
+        $stmt->bindValue(':id',     $this->__get('id'));
+        $stmt->execute();
+    } 
+
+    public function __get($atributo)
+    {
+        return $this->$atributo;
+    }
+
+    public function __set($atributo, $valor)
+    {
+        $this->$atributo = $valor;
+    }
+} 
